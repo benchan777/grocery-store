d8f92bc41bccc086ac7c3c6e471cc7eeb7fa2642
benchan777 <benchan777@gmail.com>
2021-02-11 20:23:51 -0800

    require login to view pages, add created by user id

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem
-from grocery_app.forms import GroceryStoreForm, GroceryItemForm
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app.forms import GroceryStoreForm, GroceryItemForm, SignUpForm, LoginForm
 from flask_login import login_user, logout_user, login_required, current_user
 from grocery_app import bcrypt
 
@@ -22,13 +22,15 @@ def homepage():
     return render_template('home.html', all_stores=all_stores)
 
 @main.route('/new_store', methods=['GET', 'POST'])
+@login_required
 def new_store():
     form = GroceryStoreForm()
 
     if form.validate_on_submit():
         new_store = GroceryStore(
             title = form.title.data,
-            address = form.address.data
+            address = form.address.data,
+            created_by_id = current_user.id
         )
         db.session.add(new_store)
         db.session.commit()
@@ -39,6 +41,7 @@ def new_store():
     return render_template('new_store.html', form = form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
+@login_required
 def new_item():
     form = GroceryItemForm()
 
@@ -48,7 +51,8 @@ def new_item():
             price = form.price.data,
             category = form.category.data,
             photo_url = form.photo_url.data,
-            store = form.store.data
+            store = form.store.data,
+            created_by_id = current_user.id
         )
         db.session.add(new_item)
         db.session.commit()
@@ -59,6 +63,7 @@ def new_item():
     return render_template('new_item.html', form = form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
+@login_required
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
     form = GroceryStoreForm(obj = store)
@@ -75,6 +80,7 @@ def store_detail(store_id):
     return render_template('store_detail.html', store=store, form = form)
 
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
+@login_required
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
     form = GroceryItemForm(obj = item)
