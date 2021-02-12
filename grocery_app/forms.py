e331b8f3afa018cf4787296cb317c0b02ed7b46f
benchan777 <benchan777@gmail.com>
2021-02-11 17:46:20 -0800

    add forms for signing up and logging in

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -6,28 +6,34 @@ from grocery_app.models import GroceryItem, GroceryStore, ItemCategory
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
-
-    # TODO: Add the following fields to the form class:
-    # - title - StringField
-    # - address - StringField
-    # - submit button
     title = StringField('Store Name', validators=[DataRequired(), Length(min = 3, max = 80)])
     address = StringField('Store Address', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
-
-    # TODO: Add the following fields to the form class:
-    # - name - StringField
-    # - price - FloatField
-    # - category - SelectField (specify the 'choices' param)
-    # - photo_url - StringField (use a URL validator)
-    # - store - QuerySelectField (specify the `query_factory` param)
-    # - submit button
     name = StringField('Item Name', validators=[DataRequired()])
     price = FloatField('Item Price')
     category = SelectField('Category', choices=[('DELI', 'Deli'), ('BAKERY', 'Bakery'), ('PANTRY', 'Pantry'), ('FROZEN', 'Frozen'), ('OTHER', 'Other')])
     photo_url = StringField('Photo Url', validators=[DataRequired(), URL()])
     store = QuerySelectField('Store Name', query_factory=lambda: GroceryStore.query)
     submit = SubmitField('Submit')
+
+class SignUpForm(FlaskForm):
+    """ Form for new user sign up """
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    """ Form for logging in """
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
